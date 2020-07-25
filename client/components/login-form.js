aa4e27cb4fdf4c96356799c143991a142b9c8f36
salih <salih.ozdemir@metropolsoftware.com>
2020-07-25 18:37:03 +0300

    login form component updated

--- client/components/login-form.js
+++ client/components/login-form.js
@@ -1,26 +1,19 @@
 import React from 'react'
 
-import cn from 'classnames'
-
 import FormInput from './form-input'
 import Button from './button'
+import { Logo } from './icons'
 
 import styles from './login-form.module.css'
 
-function LoginForm({ className, ...props }) {
+function LoginForm() {
   return (
-    <div className={cn(styles.loginForm, className)} {...props}>
-      <FormInput
-        label="Email"
-        errorMessage="The email is not a valid email address."
-        hasError
-      />
-      <FormInput
-        label="Password"
-        errorMessage="The email is not a valid email address."
-      />
+    <div className={styles.loginForm}>
+      <Logo className={styles.logo} />
+      <FormInput label="Username" errorMessage="Invalid username" hasError />
+      <FormInput label="Password" errorMessage="Invalid password" />
       <Button primary full className={styles.submitButton}>
-        Log In
+        Log in
       </Button>
     </div>
   )
