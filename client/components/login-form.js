489187296cdd4236a2d2dc95df6a911cd2fb3a92
salih <salih.ozdemir@metropolsoftware.com>
2020-07-27 10:56:21 +0300

    components simplified

--- client/components/login-form.js
+++ client/components/login-form.js
@@ -2,20 +2,18 @@ import React from 'react'
 
 import FormInput from './form-input'
 import Button from './button'
-import { Logo } from './icons'
 
 import styles from './login-form.module.css'
 
 function LoginForm() {
   return (
-    <div className={styles.loginForm}>
-      <Logo className={styles.logo} />
+    <form>
       <FormInput label="Username" errorMessage="Invalid username" hasError />
       <FormInput label="Password" errorMessage="Invalid password" />
       <Button primary full className={styles.submitButton}>
         Log in
       </Button>
-    </div>
+    </form>
   )
 }
 
--- client/components/signup-form.js
+++ client/components/signup-form.js
@@ -2,21 +2,19 @@ import React from 'react'
 
 import FormInput from './form-input'
 import Button from './button'
-import { Logo } from './icons'
 
 import styles from './signup-form.module.css'
 
 function SignupForm() {
   return (
-    <div className={styles.signupForm}>
-      <Logo className={styles.logo} />
+    <form>
       <FormInput label="Username" errorMessage="Invalid username" hasError />
       <FormInput label="Password" errorMessage="Invalid password" />
       <FormInput label="Confirm Password" errorMessage="Invalid password" />
       <Button primary full className={styles.submitButton}>
         Sign up
       </Button>
-    </div>
+    </form>
   )
 }
 
