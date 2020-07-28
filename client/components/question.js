35aa0f9208c9ee4a12c6ec9a9f230f0e3d826eb5
salih <salih.ozdemir@metropolsoftware.com>
2020-07-28 17:21:37 +0300

    question component created

---
+++ client/components/question.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+import Tag from '../components/tag'
+
+import styles from './question.module.css'
+
+function QuestionStats({ voteCount, answerCount, view }) {
+  return (
+    <div className={styles.statsContainer}>
+      <div className={styles.vote}>
+        <span>{voteCount}</span>
+        <p>votes</p>
+      </div>
+      <div className={styles.answer}>
+        <span>{answerCount}</span>
+        <p>answers</p>
+      </div>
+      <p className={styles.view}>{view} views</p>
+    </div>
+  )
+}
+
+function QuestionSummary({ title, text, tags, author, createdTime }) {
+  return (
+    <div className={styles.summaryContainer}>
+      <a className={styles.questionLink}>{title}</a>
+      <p className={styles.excerpt}>{text}</p>
+      <div className={styles.footer}>
+        <div className={styles.tagContainer}>
+          {tags.map((tag) => (
+            <Tag>{tag}</Tag>
+          ))}
+        </div>
+        <div className={styles.userDetails}>
+          <span>asked {createdTime}</span>
+          <a>{author}</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Question() {
+  return (
+    <div className={styles.container}>
+      <QuestionStats voteCount={21} answerCount={32} view={112} />
+      <QuestionSummary
+        title={'lorem ipsum'}
+        text={
+          'lorem ipsum question question question question question question question question'
+        }
+        tags={['javascript', 'text']}
+        author={'salihozdemir'}
+        createdTime={'1 min ago'}
+      />
+    </div>
+  )
+}
+
+export default Question
