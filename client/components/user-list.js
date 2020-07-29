5fae1611da0a16861258a104eead84402ded5834
salih <salih.ozdemir@metropolsoftware.com>
2020-07-29 23:56:57 +0300

    user list component created

---
+++ client/components/user-list.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'next/link'
+
+import USERS from '../constants/users'
+
+import styles from './user-list.module.css'
+
+const UserList = () => {
+  return (
+    <div className={styles.container}>
+      {USERS.map((user) => (
+        <div className={styles.card}>
+          <div className={styles.avatar}>
+            <Link href="/users/[username]" as={`/users/${user}`}>
+              <a>
+                <img src="https://www.gravatar.com/avatar/e514b017977ebf742a418cac697d8996?s=96&d=identicon&r=PG" />
+              </a>
+            </Link>
+          </div>
+          <div className={styles.body}>
+            <Link href="/users/[username]" as={`/users/${user}`}>
+              <a>{user}</a>
+            </Link>
+            <p>created 54 days ago</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default UserList
--- client/pages/users.js
+++ client/pages/users.js
@@ -1,12 +1,14 @@
 import Layout from '../components/Layout/layout'
 import PageTitle from '../components/Common/page-title'
 import SearchInput from '../components/Common/search-input'
+import UserList from '../components/user-list'
 
 function UsersPage() {
   return (
     <Layout extra={false}>
       <PageTitle title="Users" borderBottom={false} />
       <SearchInput placeholder="Search by user" />
+      <UserList />
     </Layout>
   )
 }
