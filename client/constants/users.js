b3bacf1bb03a75db4e86436b522ef91ba144ef5f
salih <salih.ozdemir@metropolsoftware.com>
2020-07-29 17:30:26 +0300

    dummy data added

---
+++ client/constants/users.js
@@ -0,0 +1,25 @@
+const users = [
+  'salih',
+  'ali',
+  'emre',
+  'ayse',
+  'fatma',
+  'nermin',
+  'safiye',
+  'cemil',
+  'adem',
+  'fatih',
+  'ibrahim',
+  'ikbal',
+  'onur',
+  'selim',
+  'volkan',
+  'furkan',
+  'alican',
+  'servet',
+  'ersin',
+  'doğan',
+  'eyüp'
+]
+
+export default users
