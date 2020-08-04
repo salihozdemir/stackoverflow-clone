c672a91bbba1c190a48f2ca10ed08dfde47e5337
salih <salih.ozdemir@metropolsoftware.com>
2020-08-04 12:01:44 +0300

    button group component created

---
+++ client/components/button-group/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+import Button from '../button'
+
+import styles from './style.module.css'
+
+const ButtonGroup = ({ buttons }) => {
+  const [selected, setSelected] = useState(buttons[0])
+  return (
+    <div className={styles.container}>
+      {buttons.map((button) => (
+        <Button
+          key={button}
+          className={selected === button && styles.active}
+          onClick={() => setSelected(button)}
+        >
+          {button}
+        </Button>
+      ))}
+    </div>
+  )
+}
+
+export default ButtonGroup
