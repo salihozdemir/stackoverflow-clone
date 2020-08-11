41fb6ae8f8075dcafd25e09eabc746f8b1641a6f
salih <salih.ozdemir@metropolsoftware.com>
2020-08-11 11:20:24 +0300

    formik added to auth forms

--- client/components/auth-modal/login-form/index.js
+++ client/components/auth-modal/login-form/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+
 import FormInput from '../../form-input'
 import Button from '../../button'
 
@@ -7,13 +10,60 @@ import styles from './login-form.module.css'
 
 function LoginForm() {
   return (
-    <form>
-      <FormInput label="Username" errorMessage="Invalid username" hasError />
-      <FormInput label="Password" errorMessage="Invalid password" />
-      <Button primary full className={styles.submitButton}>
-        Log in
-      </Button>
-    </form>
+    <Formik
+      initialValues={{ username: '', password: '' }}
+      onSubmit={async (values, { setStatus, resetForm }) => {
+        console.log(values)
+      }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .required('Required')
+          .max(32, 'Must be at most 32 characters long')
+          .matches(/^[a-zA-Z0-9_-]+$/, 'Contains invalid characters'),
+        password: Yup.string()
+          .required('Required')
+          .min(6, 'Must be at least 6 characters long')
+          .max(50, 'Must be at most 50 characters long')
+      })}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+      }) => (
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <FormInput
+            label="Username"
+            type="text"
+            name="username"
+            autoComplete="off"
+            value={values.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            hasError={touched.username && errors.username}
+            errorMessage={errors.username && errors.username}
+          />
+          <FormInput
+            label="Password"
+            type="password"
+            name="password"
+            autoComplete="off"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            hasError={touched.password && errors.password}
+            errorMessage={errors.password && errors.password}
+          />
+          <Button primary full className={styles.submitButton} type="submit" disabled={isSubmitting}>
+            Log in
+          </Button>
+        </form>
+      )}
+    </Formik>
   )
 }
 
--- client/components/auth-modal/signup-form/signup-form.js
+++ client/components/auth-modal/signup-form/signup-form.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+
 import FormInput from '../../form-input'
 import Button from '../../button'
 
@@ -7,14 +10,81 @@ import styles from './signup-form.module.css'
 
 function SignupForm() {
   return (
-    <form>
-      <FormInput label="Username" errorMessage="Invalid username" hasError />
-      <FormInput label="Password" errorMessage="Invalid password" />
-      <FormInput label="Confirm Password" errorMessage="Invalid password" />
-      <Button primary full className={styles.submitButton}>
-        Sign up
-      </Button>
-    </form>
+    <Formik
+      initialValues={{ username: '', password: '', passwordConfirmation: '' }}
+      onSubmit={async (values, { setStatus, resetForm }) => {
+        console.log(values)
+      }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .required('Required')
+          .max(32, 'Must be at most 32 characters long')
+          .matches(/^[a-zA-Z0-9_-]+$/, 'Contains invalid characters'),
+        password: Yup.string()
+          .required('Required')
+          .min(6, 'Must be at least 6 characters long')
+          .max(50, 'Must be at most 50 characters long'),
+        passwordConfirmation: Yup.string().oneOf(
+          [Yup.ref('password'), null],
+          'Passwords must match'
+        )
+      })}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting
+      }) => (
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <FormInput
+            label="Username"
+            type="text"
+            name="username"
+            autoComplete="off"
+            value={values.username}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            hasError={touched.username && errors.username}
+            errorMessage={errors.username && errors.username}
+          />
+          <FormInput
+            label="Password"
+            type="password"
+            name="password"
+            autoComplete="off"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            hasError={touched.password && errors.password}
+            errorMessage={errors.password && errors.password}
+          />
+          <FormInput
+            label="Password Confirm"
+            type="password"
+            name="passwordConfirmation"
+            autoComplete="off"
+            value={values.passwordConfirmation}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            hasError={touched.passwordConfirmation && errors.passwordConfirmation}
+            errorMessage={errors.passwordConfirmation && errors.passwordConfirmation}
+          />
+          <Button
+            primary
+            full
+            className={styles.submitButton}
+            disabled={isSubmitting}
+            type="submit"
+          >
+            Sign up
+          </Button>
+        </form>
+      )}
+    </Formik>
   )
 }
 
