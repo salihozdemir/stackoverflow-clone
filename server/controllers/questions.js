aa54232c091b1ece6151e3c23150a85eec082f62
salih <salih.ozdemir@metropolsoftware.com>
2020-08-17 22:55:41 +0300

    list popular tags endpoint created

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -66,6 +66,21 @@ exports.listByTags = async (req, res, next) => {
   }
 };
 
+exports.listPopulerTags = async (req, res, next) => {
+  try {
+    const tags = await Question.aggregate([
+      { $project: { tags: 1 } },
+      { $unwind: '$tags' },
+      { $group: { _id: '$tags', count: { $sum: 1 } } },
+      { $sort: { count: -1 } },
+      { $limit : 25 },
+    ]);
+    res.json(tags);
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.listByUser = async (req, res, next) => {
   try {
     const { username } = req.params;
