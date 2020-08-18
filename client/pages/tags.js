fa7f12f19f313e4b23884778402f30baa58f44f5
salih <salih.ozdemir@metropolsoftware.com>
2020-08-19 00:47:00 +0300

    tag page completed

--- client/pages/tags.js
+++ client/pages/tags.js
@@ -1,9 +1,49 @@
+import React, { useEffect, useState } from 'react'
+
+import { publicFetch } from '../util/fetcher'
+
 import Layout from '../components/layout'
 import PageTitle from '../components/page-title'
-import TagList from '../components/tags-view'
 import SearchInput from '../components/search-input'
+import TagList from '../components/tag-list'
+import TagItem from '../components/tag-list/tag-item'
+import { Spinner } from '../components/icons'
 
 function TagsPage() {
+  const [searchTerm, setSearchTerm] = useState(null)
+  const [tags, setTags] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    if (searchTerm === null) {
+      const fetchUser = async () => {
+        try {
+          const { data } = await publicFetch.get('/tags')
+          setTags(data)
+        } catch (error) {
+          console.log(error)
+        }
+      }
+
+      fetchUser()
+    } else {
+      const delayDebounceFn = setTimeout(async () => {
+        setLoading(true)
+        try {
+          const { data } = await publicFetch.get(
+            searchTerm ? `/tags/${searchTerm}` : `/tags`
+          )
+          setTags(data)
+        } catch (error) {
+          console.log(error)
+        }
+        setLoading(false)
+      }, 500)
+
+      return () => clearTimeout(delayDebounceFn)
+    }
+  }, [searchTerm])
+
   return (
     <Layout extra={false}>
       <PageTitle title="Tags" borderBottom={false}>
@@ -11,8 +51,28 @@ function TagsPage() {
         similar questions. Using the right tags makes it easier for others to
         find and answer your question.
       </PageTitle>
-      <SearchInput placeholder="Filter by tag name" autoFocus />
-      <TagList />
+      <SearchInput
+        placeholder="Filter by tag name"
+        autoFocus
+        isLoading={loading}
+        autoComplete="off"
+        type="text"
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {!tags && (
+        <div className="loading">
+          <Spinner />
+        </div>
+      )}
+
+      {tags && (
+        <TagList>
+          {tags?.map(({ count, _id }) => (
+            <TagItem key={_id} count={count}>{_id}</TagItem>
+          ))}
+        </TagList>
+      )}
     </Layout>
   )
 }
