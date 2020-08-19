aadf88bdf27bb2fe4765236a3a834616dfe5bc7e
salih <salih.ozdemir@metropolsoftware.com>
2020-08-19 22:32:49 +0300

    auth page created

---
+++ client/pages/auth.js
@@ -0,0 +1,25 @@
+import React, { useContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import { AuthContext } from '../store/auth'
+
+import LoginForm from '../components/auth-forms'
+
+const Login = () => {
+  const { isAuthenticated } = useContext(AuthContext)
+  const router = useRouter()
+
+  useEffect(() => {
+    isAuthenticated() && router.replace('/')
+  }, [isAuthenticated()])
+
+  return (
+    <div className="auth-container">
+      <div className="card">
+        <LoginForm screen="login" />
+      </div>
+    </div>
+  )
+}
+
+export default Login
