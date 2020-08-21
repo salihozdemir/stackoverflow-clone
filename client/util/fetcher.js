36a416d1daa8f560830cfe2af8fc5b94e6a9f2da
slhozdemir <salihozdemir94@gmail.com>
2020-08-21 20:54:37 +0300

    vercel json deleted

--- client/util/fetcher.js
+++ client/util/fetcher.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api'
-    : `https://${window.location.hostname}/api`
+    : `https://${process.env.SITE_NAME}/api`
 
 const publicFetch = axios.create({
   baseURL
