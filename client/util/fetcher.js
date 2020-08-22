82c71638501920f952d42374d4b57cb65e657922
salih <salih.ozdemir@metropolsoftware.com>
2020-08-22 14:21:59 +0300

    deployment configurations

--- client/util/fetcher.js
+++ client/util/fetcher.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api'
-    : `https://${process.env.SITE_NAME}./api`
+    : `https://${process.env.SITE_NAME}/api`
 
 const publicFetch = axios.create({
   baseURL
