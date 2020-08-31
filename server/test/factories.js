e7c3fd59164d955d0225910d453d54f8fc881792
salih <salih.ozdemir@metropolsoftware.com>
2020-08-31 14:08:26 +0300

    dummy files removed

--- server/test/factories.js
+++ server/test/factories.js
@@ -4,11 +4,3 @@ exports.validUser = () => ({
   username: faker.name.firstName().toLowerCase(),
   password: 'password'
 });
-
-exports.validPost = (author, category) => ({
-  title: faker.lorem.sentence(),
-  url: faker.internet.url(),
-  category,
-  author,
-  type: 'link'
-});
