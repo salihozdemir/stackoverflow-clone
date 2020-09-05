933bb915193d8259f47a2e77805124bc4451ec68
salih <salih.ozdemir@metropolsoftware.com>
2020-09-05 18:36:45 +0300

    fixed #6

--- server/middlewares/requireAuth.js
+++ server/middlewares/requireAuth.js
@@ -1,19 +1,25 @@
-const jwtDecode = require('jwt-decode');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 const requireAuth = (req, res, next) => {
   const token = req.headers.authorization;
   if (!token) {
     return res.status(401).json({ message: 'Authentication invalid.' });
   }
-  const decodedToken = jwtDecode(token.slice(7));
 
-  if (!decodedToken) {
-    return res.status(401).json({
-      message: 'There was a problem authorizing the request.'
+  try {
+    const decodedToken = jwt.verify(token.slice(7), config.jwt.secret, {
+      algorithm: 'HS256',
+      expiresIn: config.jwt.expiry
     });
-  } else {
+
     req.user = decodedToken;
     next();
+    
+  } catch (error) {
+    return res.status(401).json({
+      message: error.message
+    });
   }
 };
 
--- server/utils/authentication.js
+++ server/utils/authentication.js
@@ -1,22 +1,20 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const config = require("../config");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('../config');
 
 const createToken = (user) => {
   // Sign the JWT
   if (!user.role) {
-    throw new Error("No user role specified");
+    throw new Error('No user role specified');
   }
   return jwt.sign(
     {
       id: user._id,
       username: user.username,
-      role: user.role,
-      iss: "api.stackoverflow",
-      aud: "api.stackoverflow",
+      role: user.role
     },
     config.jwt.secret,
-    { algorithm: "HS256", expiresIn: config.jwt.expiry }
+    { algorithm: 'HS256', expiresIn: config.jwt.expiry }
   );
 };
 
@@ -44,5 +42,5 @@ const verifyPassword = (passwordAttempt, hashedPassword) => {
 module.exports = {
   createToken,
   hashPassword,
-  verifyPassword,
+  verifyPassword
 };
